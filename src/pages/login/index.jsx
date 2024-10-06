9ddeb2051af4a388fa982be9135ab86c9176fe84
Mustafa Keser <keserzade@gmail.com>
2024-10-06 18:15:27 +0300

    completed

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     return (
         <div className="login h-[100vh] bg-[#242424] text-white grid place-items-center">
             <div
-                className="flex w-full max-w-[600px] justify-center pt-10 h-full py-8 items-center flex-col bg-black  md:px-32 px-10 rounded-lg gap-10">
+                className="flex w-full max-w-[600px] justify-center pt-10 h-full py-8 items-center flex-col bg-black  md:px-24 px-6 rounded-lg gap-10">
                 <div className="flex justify-center">
                     <img className='h-[60px]' src="./twitter.png" alt="X-logo"/>
                 </div>
